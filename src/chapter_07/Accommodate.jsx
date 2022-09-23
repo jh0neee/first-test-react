9f20a7e75724a3a14aea7d5cc37a8afaa03e75c1
지현 <jh0neee@naver.com>
2022-09-24 01:44:05 +0900

    Effect Hook

--- src/chapter_07/Accommodate.jsx
+++ src/chapter_07/Accommodate.jsx
@@ -2,3 +2,34 @@ import React, { useState, useEffect } from "react";
 import useCounter from "./useCounter";
 
 const MAX_CAPACITY = 10;
+
+function Accommodate(props) {
+    const [isFull, setIsFull] = useState(false); // 용량 확인
+    const [count, increaseCount, decreaseCount] = useCounter(0);
+
+    useEffect(() => { // 의존성 배열이 없는 경우
+        console.log("=======================");
+        console.log("useEffect() is called");
+        console.log(`isFull: ${isFull}`);
+    });
+
+    useEffect(() => { // 의존성 배열이 있는 경우
+        setIsFull(count >= MAX_CAPACITY);
+        console.log(`Current count value: ${count}`);
+    }, [count]);
+
+    return(
+        <div style={{padding : 16}}>
+            <p>{`총 ${count}명 수용했습니다.`}</p>
+
+            <button onClick={increaseCount} disabled={isFull}>
+                입장
+            </button>
+            <button onClick={decreaseCount}>퇴장</button>
+
+            {isFull && <p style={{color: "red"}}>정원이 가득 찼습니다.</p>}
+        </div>
+    );
+}
+
+export default Accommodate;
