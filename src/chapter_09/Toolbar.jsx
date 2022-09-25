f51fe0ca95a17f9d3bebd3074f7673afa634bd04
지현 <jh0neee@naver.com>
2022-09-25 22:09:03 +0900

    Conditional Rendering

---
+++ src/chapter_09/Toolbar.jsx
@@ -0,0 +1,35 @@
+// LogIn LogOut button 보여주는 toolbar
+
+import React from "react";
+
+const styles = {
+    wrapper: {
+        padding: 16,
+        display: "flex",
+        flexDirection: "row",
+        borderBottom: "1px solid grey",
+    },
+    greeting: {
+        marginRight: 8,
+    },
+};
+
+function Toolbar(props) {
+    const { isLoggedIn, onClickLogin, onClickLogout } = props;
+
+    return (
+        <div style={styles.wrapper}>
+            {/* 조건부렌더링  */}
+
+            { isLoggedIn && <span style={styles.greeting}>환영합니다!</span> }
+
+            { isLoggedIn ?  (
+                <button onClick={onClickLogout}>로그아웃</button>
+            ) : (
+                <button onClick={onClickLogin}>로그인</button>
+            )}
+        </div>
+    );
+}
+
+export default Toolbar;
